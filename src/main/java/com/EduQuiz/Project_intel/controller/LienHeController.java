59acb37086d9f9c6f50a2fe1807c0608a04a6cb4
Nguyen Tung Vi <vimatau@gmail.com>
2025-11-26 16:15:57 +0700

    Cập nhật thay đổi

--- src/main/java/com/EduQuiz/Project_intel/controller/LienHeController.java
+++ src/main/java/com/EduQuiz/Project_intel/controller/LienHeController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class LienHeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "index"; // trả về file index.html trong templates
-
+    @GetMapping("/LienHe")
+    public String lienHe() {
+        return "LienHe";  // Trả về tên file LienHe.html
     }
 }
